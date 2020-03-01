299248c9df26dc79a807f162a2415affe66fc003
ffraniel <francis.whitehead@googlemail.com>
2020-03-01 16:22:32 +0000

    styled footer basic

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,24 +6,26 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <div >
-        <ul className="footer-col">
-          <li>
-            <Link to="/" >Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/login">Log In</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="footer-details">
-        <p>Website by <a href="www.franthedev.uk">Francis Whitehead</a></p>
-        <Link to="/" >
-          <img className="footer-logo" src={logo} alt="logo" />
-        </Link>
+      <div className="container">
+          <div className="footer-lists">
+            <ul className="footer-col">
+              <li>
+                <Link to="/" >Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/login">Log In</Link>
+              </li>
+            </ul>
+          </div>
+        <div className="footer-details">
+          <p>Website by <a href="www.franthedev.uk">Francis Whitehead</a></p>
+          <Link to="/" >
+            <img className="footer-logo" src={logo} alt="logo" />
+          </Link>
+        </div>
       </div>
     </footer>
   );
