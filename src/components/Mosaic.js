df1ba23933ea6561984e2767a177ac0bbd95563f
ffraniel <francis.whitehead@googlemail.com>
2020-09-30 20:24:50 +0100

    mosaic images

--- src/components/Mosaic.js
+++ src/components/Mosaic.js
@@ -1,7 +1,22 @@
 import React from "react";
 import "./Mosaic.css";
-import cape1 from "../assets/cape-1.jpg";
-import cape2 from "../assets/cape-2.jpg";
+// import cape1 from "../assets/cape-1.jpg";
+// import cape2 from "../assets/cape-2.jpg";
+import cape1 from "../assets/conference-photos/conference-1.jpg";
+import cape2 from "../assets/conference-photos/conference-2.jpg";
+import cape3 from "../assets/conference-photos/conference-3.jpg";
+import cape4 from "../assets/conference-photos/conference-4.jpg";
+import cape5 from "../assets/conference-photos/conference-5.jpg";
+import cape6 from "../assets/conference-photos/conference-6.jpg";
+import cape7 from "../assets/conference-photos/conference-7.jpg";
+import cape8 from "../assets/conference-photos/conference-8.jpg";
+import cape9 from "../assets/conference-photos/conference-9.jpg";
+import cape10 from "../assets/conference-photos/conference-10.jpg";
+import cape11 from "../assets/conference-photos/conference-11.jpg";
+import cape12 from "../assets/conference-photos/conference-12.jpg";
+import cape13 from "../assets/conference-photos/conference-13.jpg";
+import cape14 from "../assets/conference-photos/conference-14.jpg";
+import cape15 from "../assets/conference-photos/conference-15.jpg";
 
 const Mosaic = () => {
   return (
@@ -18,72 +33,67 @@ const Mosaic = () => {
       />
       <img
         className="mosaic-image"
-        src={cape1}
+        src={cape3}
         alt="taken at cape conference"
       />
       <img
         className="mosaic-image"
-        src={cape2}
+        src={cape4}
         alt="taken at cape conference"
       />
       <img
         className="mosaic-image"
-        src={cape1}
+        src={cape5}
         alt="taken at cape conference"
       />
       <img
         className="mosaic-image"
-        src={cape2}
+        src={cape6}
         alt="taken at cape conference"
       />
       <img
         className="mosaic-image"
-        src={cape1}
+        src={cape7}
         alt="taken at cape conference"
       />
       <img
         className="mosaic-image"
-        src={cape2}
+        src={cape8}
         alt="taken at cape conference"
       />
       <img
         className="mosaic-image"
-        src={cape1}
+        src={cape9}
         alt="taken at cape conference"
       />
       <img
         className="mosaic-image"
-        src={cape2}
+        src={cape10}
         alt="taken at cape conference"
       />
       <img
         className="mosaic-image"
-        src={cape1}
+        src={cape11}
         alt="taken at cape conference"
       />
       <img
         className="mosaic-image"
-        src={cape2}
+        src={cape12}
         alt="taken at cape conference"
       />
       <img
         className="mosaic-image"
-        src={cape1}
+        src={cape13}
         alt="taken at cape conference"
       />
       <img
         className="mosaic-image"
-        src={cape2}
+        src={cape14}
         alt="taken at cape conference"
       />
       <img
         className="mosaic-image"
-        src={cape1}
-        alt="taken at cape conference"
-      />
-      <img
-        className="mosaic-image"
-        src={cape2}
+        src={cape15}
         alt="taken at cape conference"
       />
     </div>
